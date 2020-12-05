8794ee40574c3e557cd97705d51fdd5bac88667e
IR~SAGE <sibo.egide98@gmail.com>
2020-12-05 20:52:43 +0200

    Add Request Permitions for Camera and Storage

--- EmployeeApp/app/src/main/java/com/irsage/employeeapp/CreateUserActivity.java
+++ EmployeeApp/app/src/main/java/com/irsage/employeeapp/CreateUserActivity.java
@@ -1,14 +1,94 @@
 package com.irsage.employeeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class CreateUserActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST_CODE=100;
+    private static final int STORAGE_REQUEST_CODE=101;
+    private static final int IMAGE_PICK_CAMERA_CODE=102;
+    private static final int IMAGE_PICK_GALLERY_CODE=103;
 
+    private String[] cameraPermissions;
+    private String[] storagePermissions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+    }
+
+
+
+
+
+
+
+
+    //check storage permission
+    private boolean checkStoragePermission(){
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)==
+                (PackageManager.PERMISSION_GRANTED);
+        return  result;
+    }
+    // request storage permissions
+    private void requestStoragePermission(){
+        ActivityCompat.requestPermissions(this,storagePermissions,STORAGE_REQUEST_CODE);
+    }
+    // check camera permission
+    private boolean checkCameraPermission(){
+        boolean result = ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)==
+                (PackageManager.PERMISSION_GRANTED);
+        boolean result1=ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)==
+                (PackageManager.PERMISSION_GRANTED);
+        return  result && result1;
+    }
+    // request camera permissions
+    private void requestCameraPermission(){
+        ActivityCompat.requestPermissions(this,cameraPermissions,CAMERA_REQUEST_CODE);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case CAMERA_REQUEST_CODE:{
+                if(grantResults.length>0){
+                    boolean cameraAccepted=grantResults[0]==PackageManager.PERMISSION_GRANTED;
+                    boolean storageAccepted=grantResults[1]== PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted && storageAccepted){
+                        pickFromCamera();
+                    }
+                    else{
+                        Toast.makeText(this, "Camera permission denied!",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+            case STORAGE_REQUEST_CODE:{
+                if (grantResults.length>0){
+                    boolean storageAccepted=grantResults[0]== PackageManager.PERMISSION_GRANTED;
+                    if(storageAccepted){
+                        PickFromStorage();
+                    }
+                    else{
+                        Toast.makeText(this, "Storage permission denied!",Toast.LENGTH_SHORT).show();
+                    }}
+            }
+        }
+
+    }
+
+    private void PickFromStorage() {
+
+    }
+
+    private void pickFromCamera() {
+
     }
 }
